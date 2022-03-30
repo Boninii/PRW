cd35810a5fcc3433b7033e4ea878486c82d4774e
bonini <wakandadoceu@gmail.com>
2022-03-30 10:13:39 -0300

    Data - Exemplo

--- aula6/exData.php
+++ aula6/exData.php
@@ -1,3 +1,5 @@
 <?php
-    echo "Hoje é dia: ".date('d/m/y')
+    echo "Hoje é dia: ".date('d/m/y')."<br>";
+    echo "Hoje é dia: ".date('d/m/Y')."<br>";
+    echo "Hoje é dia: ".date('d/m/y H:i:s')."<br><br><br>";
 ?>
