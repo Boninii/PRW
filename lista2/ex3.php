ea8e3edb0ccf643003b6d3f91f5e289abb702812
bonini <wakandadoceu@gmail.com>
2022-03-30 11:36:23 -0300

    Exercícios - Lista 2

---
+++ lista2/ex3.php
@@ -0,0 +1,13 @@
+<?php
+    for($i=0; $i<=10; $i++)
+    {
+        if($i % 2 == 0)
+        {
+            echo "O número ".$i." não é primo!<br>";
+        }
+        else
+        {
+            echo "O número ".$i." é primo!<br>";
+        }
+    }
+?>
