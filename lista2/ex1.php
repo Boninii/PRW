c09a466f8b92faf123d61da653c0086f1c1ab4db
bonini <wakandadoceu@gmail.com>
2022-03-30 11:22:19 -0300

    Exercícios - Lista 02

---
+++ lista2/ex1.php
@@ -0,0 +1,11 @@
+<?php
+    $soma = 0;
+     for($i=1;$i<=100;$i++)
+     {
+        if($i % 2 == 0)
+        {
+            $soma = $soma + $i;
+        }
+     }
+     echo "A soma dos número pares de 0 a 100 é de: ".$soma;
+?>
