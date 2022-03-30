ebc152284652b4ac04736329c429016ecad965f0
bonini <wakandadoceu@gmail.com>
2022-03-30 10:13:50 -0300

    String - Exemplo

---
+++ aula6/exString.php
@@ -0,0 +1,7 @@
+<?php
+    $palavra = " Ciência da  Computação ";
+    echo $palavra;
+    echo "<br>"."A variável contém: ".strlen($palavra)." caracteres"."<br>";
+    $palavra = trim($palavra);
+    echo "A variável contém: ".strlen($palavra)." caracteres";
+?>
