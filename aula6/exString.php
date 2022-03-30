1d1f26afe6cb46567e23d794c94a489ab199e61e
bonini <wakandadoceu@gmail.com>
2022-03-30 10:15:36 -0300

    String - Exemplo

--- aula6/exString.php
+++ aula6/exString.php
@@ -1,7 +1,7 @@
 <?php
     $palavra = " Ciência da  Computação ";
     echo $palavra;
-    echo "<br>"."A variável contém: ".strlen($palavra)." caracteres"."<br>";
-    $palavra = trim($palavra);
+    echo "<br>"."A variável contém: ".strlen($palavra)." caracteres"."<br>";//strlen fala quantos caracteres tem 
+    $palavra = trim($palavra);//Remove os espaços
     echo "A variável contém: ".strlen($palavra)." caracteres";
 ?>
