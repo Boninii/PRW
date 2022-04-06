dd4f9c4f4b248b3d91920304f51d0d6b50137005
bonini <wakandadoceu@gmail.com>
2022-04-06 10:55:25 -0300

    Exercícios - Lista 3

--- lista3/ex2.php
+++ lista3/ex2.php
@@ -1,3 +1,15 @@
 <?php
     $palavra = "banana";
+
+    $novaPal = str_replace('a', 'x', $palavra);
+    $novaPal = str_replace('e', 'x', $novaPal);
+    $novaPal= str_replace('i', 'x', $novaPal);
+    $novaPal= str_replace('o', 'x', $novaPal);
+    $novaPal = str_replace('u', 'x', $novaPal);
+    $novaPal = str_replace('A', 'x', $novaPal);
+    $novaPal = str_replace('E', 'x', $novaPal);
+    $novaPal = str_replace('I', 'x', $novaPal);
+    $novaPal= str_replace('O', 'x', $novaPal);
+    $novaPal = str_replace('U', 'x', $novaPal);
+    echo $novaPal;
 ?>
---
+++ lista3/ex3.php
@@ -0,0 +1,7 @@
+<?php
+    $cep = "16220-000";
+
+    echo $cep."<br>";
+
+    echo substr($cep, -3);
+?>
