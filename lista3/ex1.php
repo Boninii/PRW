f30607961c8fc3410402d2502dcfec6148129886
bonini <wakandadoceu@gmail.com>
2022-04-06 08:48:43 -0300

    Exercícios - Lista 3

---
+++ lista3/ex1.php
@@ -0,0 +1,10 @@
+<?php
+    $palavra1 = "banana";
+    $frase1 = "Olá, eu sou o bonini";
+
+    echo $palavra1;
+    echo "<br>"."A variável contém: ".strlen($palavra1)." caracteres"."<br>";
+
+    echo $frase1;
+    echo "<br>"."A variável contém: ".strlen($frase1)." caracteres"."<br>";
+?>
