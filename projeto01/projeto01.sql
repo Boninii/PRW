5f4391105dbf463b08dd16f400b8e61e860f77a5
bonini <wakandadoceu@gmail.com>
2022-04-20 10:22:26 -0300

    Projeto 01 - Banco de Dados

--- projeto01/projeto01.sql
+++ projeto01/projeto01.sql
@@ -9,10 +9,10 @@ use projeto01;
 
 -- Criação da tabela usuario
 create table usuario(
-	id_usuario 		 int 		  not null auto_increment,
-    nome_usuario 	 varchar(100) not null,
-    email_usuario 	 varchar(100) not null,
-    telefone_usuario varchar(30)  not null,
+	id_usuario int not null auto_increment,
+    nome_usuario varchar(100) not null,
+    email_usuario varchar(100) not null,
+    telefone_usuario varchar(30) not null,
     primary key (id_usuario)
-);
+    );
     
