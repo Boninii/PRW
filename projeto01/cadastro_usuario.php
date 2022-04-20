3e5671554d89f94fc83ea39b58aaf129238d128e
bonini <wakandadoceu@gmail.com>
2022-04-20 09:25:35 -0300

    Projeto 01 - Banco de Dados

---
+++ projeto01/cadastro_usuario.php
@@ -0,0 +1,9 @@
+<?php
+    $nome_usuario = $_POST['textNome'];
+    $email_usuario = $_POST['email'];
+    $fone_usuario = $_POST['fone'];
+
+    echo "Nome do usuário: ".$nome_usuario."<br>"
+    ."Email: ".$email_usuario."<br>"
+    ."Telefone: ".$fone_usuario;
+?>
