5c53ee19b429c85978bb65e20d676404a4cc438d
bonini <bonini>
2022-04-27 20:28:25 -0300

    Projeto 01 - Banco de Dados

--- projeto01/sql/teste02.sql
+++ projeto01/sql/teste02.sql
@@ -5,10 +5,10 @@ INSERT INTO  usuario(nome_usuario, email_usuario, telefone_usuario)
 	VALUES('Pedro', 'pedroboninibernardi@gmail.com', '(18)99795-4012');
 */
 
-/*
+/**
 UPDATE usuario 
-SET id_usuario = 3
-where id_usuario = 8;
+SET id_usuario = 6
+where id_usuario = 11;
 */
 
 /*
