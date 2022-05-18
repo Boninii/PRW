135198dfc7ac820619caf917d26630d0968fb1b5
bonini <wakandadoceu@gmail.com>
2022-05-18 09:10:56 -0300

    Lista 6 - Projeto 02

---
+++ lista6/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html lang="pt_BR">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Menu Principal</title>
+        </head>
+        <body>
+            <h1>Menu Principal</h1>
+            <div>
+                <ul>
+                    <li><a href="cadastro_usuario.html">Cadastrar Agenda</a></li>
+                    <li><a href="listar_usuarios.php">Listar Agenda</a></li>
+                </ul>
+            </div>
+        </body>
+    </html>
