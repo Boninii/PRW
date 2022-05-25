1288c3d1ce348f45af45825b3850c3d979c5f716
bonini <wakandadoceu@gmail.com>
2022-05-25 09:34:57 -0300

    Projeto 02 - Banco de Dados

--- projeto02/sql/projeto01.sql
+++ projeto02/sql/projeto01.sql
@@ -1,11 +1,11 @@
 -- Drop banco de dados
--- drop database projeto01
+-- drop database projeto02
 
 -- Criação do banco de dados 
-create database projeto01;
+create database projeto02;
 
 -- Selecionar banco de dados 
-use projeto01;
+use projeto02;
 
 -- Criação da tabela usuario
 create table usuario(
@@ -13,6 +13,8 @@ create table usuario(
     nome_usuario varchar(100) not null,
     email_usuario varchar(100) not null,
     telefone_usuario varchar(30) not null,
+    foto_blob blob,
+    foto_nome varchar(100),
     primary key (id_usuario)
 );
     
