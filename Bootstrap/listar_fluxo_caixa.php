fca7d4282dd4405d8b28c1651c1319d5363a140e
bonini <wakandadoceu@gmail.com>
2022-06-15 11:47:19 -0300

    Bootstrap

--- Bootstrap/listar_fluxo_caixa.php
+++ Bootstrap/listar_fluxo_caixa.php
@@ -24,20 +24,42 @@
             <title>Bootstrap</title>
         </head>
         <body>
-            <h2 style="text-align: center;">Consulta de fluxo de caixa</h2>
-            <div>
-                <ul class="nav justify-content-center">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="cadastro_fluxo_caixa.html">Cadastro</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="listar_fluxo_caixa.php">Listagem</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="consulta_fluxo_caixa.html">Consulta</a>
-                    </li>
-                </ul>
-            </div>     
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#" style="pointer-events: none;">Menu Principal</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="cadastro_fluxo_caixa.html">Cadastro<span class="sr-only">(página atual)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="listar_fluxo_caixa.php">Listagem</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="consulta_fluxo_caixa.html">Consulta</a>
+                        </li>
+                        <!--<li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Ação</a>
+                                <a class="dropdown-item" href="#">Outra ação</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Algo mais aqui</a>
+                            </div>
+                        </li>-->
+                    </ul>
+                    <!--<form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                    </form>-->
+                </div>
+            </nav>
+            <br>     
             <table align="center" border="1">
                 <!--LInha-->
                 <tr align="center">
