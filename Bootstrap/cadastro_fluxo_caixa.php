9b725330440b2c289814c28269b494f1b4dad8fd
bonini <wakandadoceu@gmail.com>
2022-06-22 10:10:55 -0300

    Bootstrap

--- Bootstrap/cadastro_fluxo_caixa.php
+++ Bootstrap/cadastro_fluxo_caixa.php
@@ -1,6 +1,27 @@
 <?php
     include('conexao.php');
 
+    //Upload da foto
+    $fotoNome = $_FILES['foto']['name'];
+    $target_dir = "upload/";
+    $target_file = $target_dir . basename($_FILES["foto"]["name"]);
+
+    //Select file type
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+    //Valid file extensions
+    $extensions_arr = array("jpg", "jpeg", "png", "gif");
+
+
+    //Check entension
+    if(in_array($imageFileType, $extensions_arr))
+    {
+        if(move_uploaded_file($_FILES['foto']['tmp_name'], $target_dir.$fotoNome))
+        {
+            $fotoBlob = addslashes(file_get_contents($target_dir.$fotoNome));
+        }
+    }
+
     $data = $_POST['data'];
     $tipo = $_POST['opcTipo'];
     $valor = $_POST['valor'];
@@ -13,8 +34,8 @@
     ."Histórico: ".$historico."<br>"
     ."Cheque: ".$cheque;
 
-    $sql = "INSERT INTO  fluxo_caixa (data, tipo, valor, historico, cheque)
-                VALUES ('".$data."', '".$tipo."', '".$valor."', '".$historico."', '".$cheque."')";
+    $sql = "INSERT INTO  fluxo_caixa (data, tipo, valor, historico, cheque, foto_blob, foto_nome)
+                VALUES ('".$data."', '".$tipo."', '".$valor."', '".$historico."', '".$cheque."', '".$fotoBlob."', '".$fotoNome."')";
 
     $result = mysqli_query($con, $sql);
 
--- Bootstrap/listar_fluxo_caixa.php
+++ Bootstrap/listar_fluxo_caixa.php
@@ -21,6 +21,7 @@
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+            <link rel="stylesheet" href="css/listar_fluxo_caixa.css">
             <title>Bootstrap</title>
         </head>
         <body>
@@ -70,23 +71,27 @@
                     <th>Valor</th>
                     <th>Histórico</th>
                     <th>Cheque</th>
+                    <th>Foto</th>
                     <th>Excluir</th>
                 </tr>
                 <?php
                     while($row = mysqli_fetch_array($result))
                     {
                         echo "<tr>";
-                        echo "<td>".$row['id']."</td>"; 
+                        echo "<td><a href='altera_fluxo_caixa.php?id=".$row['id']."'>"
+                            .$row['id']."</a></td>";
                         echo "<td>".$row['data']."</td>";
                         echo "<td>".$row['tipo']."</td>";
                         echo "<td>".$row['valor']."</td>";
-                        echo "<td><a href='altera_fluxo_caixa.php?id=".$row['id']."'>".$row['historico']."</a></td>";
+                        echo "<td>".$row['historico']."</td>";
                         echo "<td>".$row['cheque']."</td>";
+                        echo "<td><img src='data:image/jpeg;base64,".base64_encode( $row["foto_blob"] ).
+                        "' width='150' height='150'/></td>";
                         echo "<td><a href='excluir_fluxo_caixa.php?id=".$row['id']."'>Excluir</a></td>";
                         echo "</tr>";
                     }
                 ?>
-                <td colspan="7" style="text-align: center;"><a href='index.php'>Voltar</a></td>
+                <td colspan="8" style="text-align: center;"><a href='index.php'>Voltar</a></td>
             </table>
         </body>
     </html>
